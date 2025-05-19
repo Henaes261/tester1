dc0476cbb18813e41e000d53aa5a6e2eb8eea76e
tagiang1254 <taquynhgiang05122004@gmail.com>
2025-05-19 16:19:15 +0700

    Update FrmTimKiemHoaDonNhapHang.Designer.cs

--- QLXM/FrmTimKiemHoaDonNhapHang.Designer.cs
+++ QLXM/FrmTimKiemHoaDonNhapHang.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QLXM
+namespace QLXM
 {
     partial class FrmTimKiemHoaDonNhapHang
     {
@@ -28,12 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboMaHDN = new System.Windows.Forms.ComboBox();
+            this.cboMaNCC = new System.Windows.Forms.ComboBox();
+            this.cboMaNV = new System.Windows.Forms.ComboBox();
+            this.cboNam = new System.Windows.Forms.ComboBox();
+            this.cboThang = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTimkiem = new System.Windows.Forms.Button();
+            this.btnTimlai = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tìm kiếm hoá đơn nhập hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã hoá đơn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã nhà cung cấp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(237, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Năm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tháng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(414, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Mã nhân viên";
+            // 
+            // cboMaHDN
+            // 
+            this.cboMaHDN.FormattingEnabled = true;
+            this.cboMaHDN.Location = new System.Drawing.Point(148, 94);
+            this.cboMaHDN.Name = "cboMaHDN";
+            this.cboMaHDN.Size = new System.Drawing.Size(230, 28);
+            this.cboMaHDN.TabIndex = 6;
+            // 
+            // cboMaNCC
+            // 
+            this.cboMaNCC.FormattingEnabled = true;
+            this.cboMaNCC.Location = new System.Drawing.Point(551, 91);
+            this.cboMaNCC.Name = "cboMaNCC";
+            this.cboMaNCC.Size = new System.Drawing.Size(227, 28);
+            this.cboMaNCC.TabIndex = 7;
+            // 
+            // cboMaNV
+            // 
+            this.cboMaNV.FormattingEnabled = true;
+            this.cboMaNV.Location = new System.Drawing.Point(551, 156);
+            this.cboMaNV.Name = "cboMaNV";
+            this.cboMaNV.Size = new System.Drawing.Size(227, 28);
+            this.cboMaNV.TabIndex = 8;
+            // 
+            // cboNam
+            // 
+            this.cboNam.FormattingEnabled = true;
+            this.cboNam.Location = new System.Drawing.Point(298, 164);
+            this.cboNam.Name = "cboNam";
+            this.cboNam.Size = new System.Drawing.Size(80, 28);
+            this.cboNam.TabIndex = 9;
+            this.cboNam.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cboNam_KeyPress);
+            // 
+            // cboThang
+            // 
+            this.cboThang.FormattingEnabled = true;
+            this.cboThang.Location = new System.Drawing.Point(148, 164);
+            this.cboThang.Name = "cboThang";
+            this.cboThang.Size = new System.Drawing.Size(68, 28);
+            this.cboThang.TabIndex = 10;
+            this.cboThang.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cboThang_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(53, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 171);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnTimkiem
+            // 
+            this.btnTimkiem.Location = new System.Drawing.Point(140, 417);
+            this.btnTimkiem.Name = "btnTimkiem";
+            this.btnTimkiem.Size = new System.Drawing.Size(75, 41);
+            this.btnTimkiem.TabIndex = 12;
+            this.btnTimkiem.Text = "Tìm HĐ";
+            this.btnTimkiem.UseVisualStyleBackColor = true;
+            this.btnTimkiem.Click += new System.EventHandler(this.btnTimkiem_Click);
+            // 
+            // btnTimlai
+            // 
+            this.btnTimlai.Location = new System.Drawing.Point(356, 417);
+            this.btnTimlai.Name = "btnTimlai";
+            this.btnTimlai.Size = new System.Drawing.Size(75, 41);
+            this.btnTimlai.TabIndex = 13;
+            this.btnTimlai.Text = "Tìm lại";
+            this.btnTimlai.UseVisualStyleBackColor = true;
+            this.btnTimlai.Click += new System.EventHandler(this.btnTimlai_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(587, 417);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(75, 41);
+            this.btnDong.TabIndex = 14;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // FrmTimKiemHoaDonNhapHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnTimlai);
+            this.Controls.Add(this.btnTimkiem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cboThang);
+            this.Controls.Add(this.cboNam);
+            this.Controls.Add(this.cboMaNV);
+            this.Controls.Add(this.cboMaNCC);
+            this.Controls.Add(this.cboMaHDN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmTimKiemHoaDonNhapHang";
             this.Text = "FrmTimKiemHoaDonNhapHang";
+            this.Load += new System.EventHandler(this.FrmTimKiemHoaDonNhapHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboMaHDN;
+        private System.Windows.Forms.ComboBox cboMaNCC;
+        private System.Windows.Forms.ComboBox cboMaNV;
+        private System.Windows.Forms.ComboBox cboNam;
+        private System.Windows.Forms.ComboBox cboThang;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTimkiem;
+        private System.Windows.Forms.Button btnTimlai;
+        private System.Windows.Forms.Button btnDong;
     }
 }
