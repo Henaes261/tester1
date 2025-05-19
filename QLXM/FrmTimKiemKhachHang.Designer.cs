438dfbe15f45dc0fc1c6b8554bbd0a57d235c6a3
phunghien222 <phungthuyhien222@gmail.com>
2025-05-19 20:25:32 +0700

    Update FrmTimKiemKhachHang.Designer.cs

--- QLXM/FrmTimKiemKhachHang.Designer.cs
+++ QLXM/FrmTimKiemKhachHang.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QLXM
+namespace QLXM
 {
     partial class FrmTimKiemKhachHang
     {
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvTKKH = new System.Windows.Forms.DataGridView();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.cboMaKhach = new System.Windows.Forms.ComboBox();
+            this.txtTenKhach = new System.Windows.Forms.TextBox();
+            this.cboDiaChi = new System.Windows.Forms.ComboBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.btnTimKiemLai = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTKKH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(258, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÌM KIẾM KHÁCH HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã khách";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên khách";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Số điện thoại";
+            // 
+            // dgvTKKH
+            // 
+            this.dgvTKKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTKKH.Location = new System.Drawing.Point(74, 228);
+            this.dgvTKKH.Name = "dgvTKKH";
+            this.dgvTKKH.RowHeadersWidth = 51;
+            this.dgvTKKH.RowTemplate.Height = 24;
+            this.dgvTKKH.Size = new System.Drawing.Size(646, 150);
+            this.dgvTKKH.TabIndex = 5;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(406, 396);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 6;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(645, 396);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // cboMaKhach
+            // 
+            this.cboMaKhach.FormattingEnabled = true;
+            this.cboMaKhach.Location = new System.Drawing.Point(175, 88);
+            this.cboMaKhach.Name = "cboMaKhach";
+            this.cboMaKhach.Size = new System.Drawing.Size(192, 24);
+            this.cboMaKhach.TabIndex = 8;
+            // 
+            // txtTenKhach
+            // 
+            this.txtTenKhach.Location = new System.Drawing.Point(175, 120);
+            this.txtTenKhach.Name = "txtTenKhach";
+            this.txtTenKhach.Size = new System.Drawing.Size(334, 22);
+            this.txtTenKhach.TabIndex = 9;
+            // 
+            // cboDiaChi
+            // 
+            this.cboDiaChi.FormattingEnabled = true;
+            this.cboDiaChi.Location = new System.Drawing.Point(175, 148);
+            this.cboDiaChi.Name = "cboDiaChi";
+            this.cboDiaChi.Size = new System.Drawing.Size(334, 24);
+            this.cboDiaChi.TabIndex = 11;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(175, 181);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(192, 22);
+            this.txtSDT.TabIndex = 12;
+            // 
+            // btnTimKiemLai
+            // 
+            this.btnTimKiemLai.Location = new System.Drawing.Point(515, 396);
+            this.btnTimKiemLai.Name = "btnTimKiemLai";
+            this.btnTimKiemLai.Size = new System.Drawing.Size(97, 23);
+            this.btnTimKiemLai.TabIndex = 13;
+            this.btnTimKiemLai.Text = "Tìm kiếm lại";
+            this.btnTimKiemLai.UseVisualStyleBackColor = true;
+            this.btnTimKiemLai.Click += new System.EventHandler(this.btnTimKiemLai_Click);
+            // 
+            // FrmTimKiemKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmTimKiemKhachHang";
+            this.Controls.Add(this.btnTimKiemLai);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.cboDiaChi);
+            this.Controls.Add(this.txtTenKhach);
+            this.Controls.Add(this.cboMaKhach);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.dgvTKKH);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmTimKiemKhachHang";
+            this.Text = "Tìm kiếm Khách hàng";
+            this.Load += new System.EventHandler(this.FrmTimKiemKhachHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTKKH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvTKKH;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ComboBox cboMaKhach;
+        private System.Windows.Forms.TextBox txtTenKhach;
+        private System.Windows.Forms.ComboBox cboDiaChi;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Button btnTimKiemLai;
     }
 }
