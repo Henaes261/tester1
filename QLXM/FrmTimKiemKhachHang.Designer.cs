291f1fbc2d44eba2c58ba2ba938c4858bfadedf5
tagiang1254 <taquynhgiang05122004@gmail.com>
2025-05-24 00:28:20 +0700

    Update FrmTimKiemKhachHang.Designer.cs

--- QLXM/FrmTimKiemKhachHang.Designer.cs
+++ QLXM/FrmTimKiemKhachHang.Designer.cs
@@ -48,63 +48,65 @@ namespace QLXM
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(258, 26);
+            this.label1.Location = new System.Drawing.Point(279, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 25);
+            this.label1.Size = new System.Drawing.Size(359, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "TÌM KIẾM KHÁCH HÀNG";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(71, 97);
+            this.label2.Location = new System.Drawing.Point(80, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.Size = new System.Drawing.Size(78, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mã khách";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(71, 127);
+            this.label3.Location = new System.Drawing.Point(80, 159);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.Size = new System.Drawing.Size(83, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Tên khách";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(71, 157);
+            this.label4.Location = new System.Drawing.Point(80, 196);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.Size = new System.Drawing.Size(57, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Địa chỉ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(71, 188);
+            this.label5.Location = new System.Drawing.Point(80, 235);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.Size = new System.Drawing.Size(102, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Số điện thoại";
             // 
             // dgvTKKH
             // 
             this.dgvTKKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTKKH.Location = new System.Drawing.Point(74, 228);
+            this.dgvTKKH.Location = new System.Drawing.Point(70, 286);
+            this.dgvTKKH.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvTKKH.Name = "dgvTKKH";
             this.dgvTKKH.RowHeadersWidth = 51;
             this.dgvTKKH.RowTemplate.Height = 24;
-            this.dgvTKKH.Size = new System.Drawing.Size(646, 150);
+            this.dgvTKKH.Size = new System.Drawing.Size(727, 188);
             this.dgvTKKH.TabIndex = 5;
             // 
             // btnTimKiem
             // 
-            this.btnTimKiem.Location = new System.Drawing.Point(406, 396);
+            this.btnTimKiem.Location = new System.Drawing.Point(159, 495);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.Size = new System.Drawing.Size(84, 29);
             this.btnTimKiem.TabIndex = 6;
             this.btnTimKiem.Text = "Tìm kiếm";
             this.btnTimKiem.UseVisualStyleBackColor = true;
@@ -112,9 +114,10 @@ namespace QLXM
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(645, 396);
+            this.btnThoat.Location = new System.Drawing.Point(554, 495);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.Size = new System.Drawing.Size(84, 29);
             this.btnThoat.TabIndex = 7;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -123,38 +126,43 @@ namespace QLXM
             // cboMaKhach
             // 
             this.cboMaKhach.FormattingEnabled = true;
-            this.cboMaKhach.Location = new System.Drawing.Point(175, 88);
+            this.cboMaKhach.Location = new System.Drawing.Point(197, 110);
+            this.cboMaKhach.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cboMaKhach.Name = "cboMaKhach";
-            this.cboMaKhach.Size = new System.Drawing.Size(192, 24);
+            this.cboMaKhach.Size = new System.Drawing.Size(216, 28);
             this.cboMaKhach.TabIndex = 8;
             // 
             // txtTenKhach
             // 
-            this.txtTenKhach.Location = new System.Drawing.Point(175, 120);
+            this.txtTenKhach.Location = new System.Drawing.Point(197, 150);
+            this.txtTenKhach.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtTenKhach.Name = "txtTenKhach";
-            this.txtTenKhach.Size = new System.Drawing.Size(334, 22);
+            this.txtTenKhach.Size = new System.Drawing.Size(375, 26);
             this.txtTenKhach.TabIndex = 9;
             // 
             // cboDiaChi
             // 
             this.cboDiaChi.FormattingEnabled = true;
-            this.cboDiaChi.Location = new System.Drawing.Point(175, 148);
+            this.cboDiaChi.Location = new System.Drawing.Point(197, 185);
+            this.cboDiaChi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cboDiaChi.Name = "cboDiaChi";
-            this.cboDiaChi.Size = new System.Drawing.Size(334, 24);
+            this.cboDiaChi.Size = new System.Drawing.Size(375, 28);
             this.cboDiaChi.TabIndex = 11;
             // 
             // txtSDT
             // 
-            this.txtSDT.Location = new System.Drawing.Point(175, 181);
+            this.txtSDT.Location = new System.Drawing.Point(197, 226);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtSDT.Name = "txtSDT";
-            this.txtSDT.Size = new System.Drawing.Size(192, 22);
+            this.txtSDT.Size = new System.Drawing.Size(216, 26);
             this.txtSDT.TabIndex = 12;
             // 
             // btnTimKiemLai
             // 
-            this.btnTimKiemLai.Location = new System.Drawing.Point(515, 396);
+            this.btnTimKiemLai.Location = new System.Drawing.Point(344, 495);
+            this.btnTimKiemLai.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnTimKiemLai.Name = "btnTimKiemLai";
-            this.btnTimKiemLai.Size = new System.Drawing.Size(97, 23);
+            this.btnTimKiemLai.Size = new System.Drawing.Size(109, 29);
             this.btnTimKiemLai.TabIndex = 13;
             this.btnTimKiemLai.Text = "Tìm kiếm lại";
             this.btnTimKiemLai.UseVisualStyleBackColor = true;
@@ -162,9 +170,9 @@ namespace QLXM
             // 
             // FrmTimKiemKhachHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(841, 562);
             this.Controls.Add(this.btnTimKiemLai);
             this.Controls.Add(this.txtSDT);
             this.Controls.Add(this.cboDiaChi);
@@ -178,6 +186,7 @@ namespace QLXM
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmTimKiemKhachHang";
             this.Text = "Tìm kiếm Khách hàng";
             this.Load += new System.EventHandler(this.FrmTimKiemKhachHang_Load);
